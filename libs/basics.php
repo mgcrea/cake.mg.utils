803321026390c3a96f2684f905f68d2e16e36bd0
Olivier Louvignes <olivier@mg-crea.com>
2011-01-22 02:22:39 +0100

    Improved create_guid() function.

--- libs/basics.php
+++ libs/basics.php
@@ -379,8 +379,16 @@
 		return $subject;
 	}
 
+/**
+ * Creates a global unique identifier
+ *
+ * @return string guid
+ */
 	function create_guid() {
-		return strtolower(substr(substr(com_create_guid(), 1), 0, 36));
+		 if (function_exists('com_create_guid') === true) {
+			return trim(com_create_guid(), '{}');
+		}
+		return strtolower(sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535)));
 	}
 
 /**
